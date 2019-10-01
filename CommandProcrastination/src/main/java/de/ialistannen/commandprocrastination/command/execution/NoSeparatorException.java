3eb6c68c3e791508631b0387852b9502b128cce4
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2019-10-01 20:10:47 +0200

    Add javadoc to NoSeparatorException

--- CommandProcrastination/src/main/java/de/ialistannen/commandprocrastination/command/execution/NoSeparatorException.java
+++ CommandProcrastination/src/main/java/de/ialistannen/commandprocrastination/command/execution/NoSeparatorException.java
@@ -3,6 +3,9 @@ package de.ialistannen.commandprocrastination.command.execution;
 import de.ialistannen.commandprocrastination.parsing.ParseException;
 import de.ialistannen.commandprocrastination.util.StringReader;
 
+/**
+ * Thrown when a command misses a separator in the command executor.
+ */
 public class NoSeparatorException extends ParseException {
 
   public NoSeparatorException(StringReader reader) {
